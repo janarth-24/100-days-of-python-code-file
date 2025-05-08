3d9b5ea6caae9d4bd661756ffb4b5fd00be619ff
Janarthanan <janarthanan0012@gmail.com>
2025-05-08 20:02:51 +0530

    day 3

---
+++ Treasure_Island_project.py
@@ -0,0 +1,44 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+print("You're at a cross road. Where do you want to go? ")
+direction= input('Type "left" or "right" ')
+if direction == "left":
+    print("You've come to a lake. There is an island in the middle of the lake.")
+    user_input=input('Type "wait" to wait for a boat. Type "swim" to swim across. ')
+    if user_input == "wait":
+        print("You arrive at the island unharmed. There is a house with 3 doors.")
+        choose_color=input("One red, one yellow and one blue. Which colour do you choose? ")
+        if choose_color == "yellow":
+            print("You found the treasure! You Win!")
+        elif choose_color == "red":
+            print("It's a room full of fire. Game Over.")
+        elif choose_color == "blue":
+            print("You enter a room of beasts. Game Over.")
+        else:
+            print("You enter a invaild input. Game over..")
+    else:
+        print("Attacked by trout.Game Over.")
+else:
+    print("Fall into a hole.Game Over.")
